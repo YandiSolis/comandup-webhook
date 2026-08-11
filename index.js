f4b190147f4a3970422caad08195becfba0c0072
Yandi Solis <yandimeli52@gmail.com>
2026-08-11 00:01:59 -0600

    Versión inicial de la página web de ComandUp

--- index.js
+++ index.js
@@ -83,7 +83,6 @@ app.post('/webhook/whatsapp', async (req, res) => {
 // ==========================================
 // 3. CORTE DE CAJA AUTOMÁTICO (El Reloj)
 // ==========================================
-// MODO PRUEBA: '* * * * *' = El reporte se enviará CADA MINUTO exacto.
 cron.schedule('0 22 * * *', async () => {
     console.log("⏰ ¡Reloj activado! Generando cierre de caja automático...");
     const textoCierre = `🌙 *CIERRE DE TURNO COMANDUP* 🌙\n\nEl turno ha finalizado de manera automática. Resumen de hoy:\n\n🧾 *Total de tickets:* ${ticketsAtendidos}\n💰 *Ingresos totales:* $${ventasDelDia.toFixed(2)}\n\n_La caja virtual ha sido reiniciada para mañana. ¡Buen descanso!_`;
@@ -94,7 +93,7 @@ cron.schedule('0 22 * * *', async () => {
         await axios.post(urlGreenAPI, { chatId: process.env.PHONE_GERENTE, message: textoCierre });
         console.log("✅ Corte de caja automático enviado a WhatsApp.");
 
-        // ¡Vaciamos la alcancía para el día siguiente!
+        // Vaciamos para el día siguiente
         ventasDelDia = 0;
         ticketsAtendidos = 0;
 
